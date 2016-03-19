1535aaba520cf513c8f749b9070de46ce1c6fa8f
Jagb <Jagb.dot@gmail.com>
2016-03-19 23:49:52 +0100

    third step: Get the values of the inputs id's with val()

--- app/assets/javascripts/feedonate.js
+++ app/assets/javascripts/feedonate.js
@@ -14,9 +14,14 @@ $( document ).ready(function() {
 
   $('#new_product').submit(function(){
     event.preventDefault()
-
+    // Setting the route and the post method
     var action = $(this).attr('action');
     var method = $(this).attr('method');
+    // get the values of the inputs
+    var name = $(this).find('#donation_product_name').val();
+    var amount = $(this).find('#donation_product_amount').val();
+    var category = $(this).find('#donation_product_category').val();
+
     // this debugger should be hit when you click the submit button!
     debugger;
   });
