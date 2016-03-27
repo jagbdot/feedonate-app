6f4c9e0c526dbfed0eb7f9eb08fcadf5d3a8acf9
Jagb <Jagb.dot@gmail.com>
2016-03-27 16:33:04 +0200

    seeds implemented for the right lecture of the project when download

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,28 @@
-10.times do |index|
-  user = User.create(
-    name: "Usuario #{index}",
+10.times do |i|
+  user = User.new(
+    name: "Donante #{i}",
     password: 12345678,
     password_confirmation: 12345678,
-    email: "#{index}@prueba.com",
+    email: "#{i}@donanteprueba.com",
+    type_of_user: "donor",
+    )
+  user.save
+  donation = user.donor_donations.new(
+    description: "Donación #{i}",
+    )
+  donation.save
+  product = donation.products.create(
+    name: "Producto #{i}",
+    )
+  product.save
+end
+
+10.times do |index|
+  bank = User.create(
+    name: "Banco #{index}",
+    password: 12345678,
+    password_confirmation: 12345678,
+    email: "#{index}@bancoprueba.com",
     type_of_user: "bank",
   )
 end
-
